9435e3e88eecaf5fb979d6a078ffc7984519f51f
itssunnyfunny <sunnyfunny4062@gmail.com>
2023-12-28 11:08:00 +0530

    testing react

---
+++ vite-react/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import Test from "./test"
+
+
+function App() {
+  
+
+  return (
+    <>
+    < Test/>
+    <h1> ye App me likha tha maine!</h1>
+    <p>h1 ki size me difference hai!</p>
+    </>
+  )
+}
+
+export default App
---
+++ vite-react/src/Test.jsx
@@ -0,0 +1,8 @@
+ function Test(){
+
+   return(
+    <h1>tis just a testing for react!</h1>
+   )
+
+ }
+ export default Test
