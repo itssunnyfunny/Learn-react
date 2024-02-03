29fe4d246cfc95904f262855e315ad9166827d6c
itssunnyfunny <sunnyfunny4062@gmail.com>
2024-02-03 12:36:45 +0530

    first-ContextAPI-project

---
+++ 08-contextApi/src/App.jsx
@@ -0,0 +1,17 @@
+import React,{useContext} from "react"
+import UserContextProvider from "./Context/UserContextProvider"
+import Login from "./Components/Login"
+import Profile from "./Components/Profile"
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1> just for context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08-contextApi/src/Components/Login.jsx
@@ -0,0 +1,38 @@
+import React ,{useState, useContext} from "react";
+import UserContext from "../Context/UserContext";
+
+
+
+function Login() {
+    const [username, setUsername] = useState('')
+    const [Passward, setPassward] = useState('')
+
+    const {setUser} = useContext(UserContext)
+
+    const handleSubmit  = (e) => {
+        e.preventDefault();
+         setUser({username, Passward})
+
+    }
+  return (
+   
+    <div>
+        <h2>Login</h2>
+        <input type="text"
+         placeholder="username"
+         value={username} 
+         onChange={(e) => setUsername(e.target.value)}/>
+         <input type="text"
+         placeholder="password" 
+         value={Passward}
+         onChange={(e) => setPassward(e.target.value)}/>
+
+         <button
+          onClick={handleSubmit}
+          >Submit</button>
+         
+    </div>
+  )
+}
+
+export default Login
